0fa3be54ea2bcee8895278361b85c3e4ebfe25ea
halil <halilakgun0@gmail.com>
2023-02-22 22:25:41 +0300

    v4

--- src/main/java/ATMApp.java
+++ src/main/java/ATMApp.java
@@ -3,5 +3,25 @@ public class ATMApp {
         System.out.println("deneme");
         System.out.println("deneme2");
         System.out.println("deneme3");
+
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        boolean isContinue = true;
+        int sum = 0;
+        int loop = 1;
+        int idx = 0;
+        while (isContinue) {
+            for (int i = 0; i < loop; i++) {
+                if (idx == arr.length - 1) {
+                    isContinue = false;
+                    break;
+                }
+                sum += arr[idx];
+                idx++;
+            }
+            System.out.println(sum);
+            sum = 0;
+            loop++;
+        }
     }
 }
