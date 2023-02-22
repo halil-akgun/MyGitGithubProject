56ffd052a2f1225fa36b855c51280593c2a55686
halil <halilakgun0@gmail.com>
2023-02-22 21:35:29 +0300

    v2

--- src/main/java/ATMApp.java
+++ src/main/java/ATMApp.java
@@ -2,5 +2,6 @@ public class ATMApp {
     public static void main(String[] args) {
         System.out.println("deneme");
         System.out.println("deneme2");
+        System.out.println("deneme3");
     }
 }
